64473c2c300bea5e82552a69d16c414469faf832
Luke Perich <luke@lpdev.io>
2020-06-26 17:13:18 +1000

    Added GTM

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,31 @@
 import 'styles/global.scss';
+import TagManager from 'react-gtm-module';
 
-export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-      
-      <footer>
-        <a href="https://lpdev.io" target="_blank">
-          &copy; LPDev {new Date().getFullYear()}
-        </a>
-      </footer>
-    </>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    /* Google Tag Manager */
+    const tagManagerArgs = {
+      gtmId: 'GTM-KDQ9GWC'
+    };
+    TagManager.initialize(tagManagerArgs);
+  }
+  
+  render() {
+    let Component = this.props.Component;
+    let pageProps = this.props.pageProps;
+
+    return (
+      <>
+        <Component {...pageProps} />
+
+        <footer>
+          <a href="https://lpdev.io" target="_blank">
+            &copy; LPDev {new Date().getFullYear()}
+          </a>
+        </footer>
+      </>
+    );
+  }
 }
+
+export default App;
