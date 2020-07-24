e25bf30b0d738b21df83ffbd29aaa86e2c49a8e8
Luke Perich <luke@lpdev.io>
2020-07-24 11:26:53 +1000

    Changed input area accent color to purple

--- pages/dash.js
+++ pages/dash.js
@@ -11,19 +11,13 @@ function Dashboard() {
 
       <Center bgColor="#2A2A2E">
         <div className="dash-wrapper">
-
-          <Input
-            id="search"
-            className="dash-search"
-            mono
-          />
-
+          <Input id="search" className="dash-search" mono />
         </div>
       </Center>
 
       <style jsx>{`
         .dash-wrapper {
-          background: #2A2A2E;
+          background: #2a2a2e;
           color: #f7f7f7;
           width: 100%;
           padding: 15vmin;
@@ -37,7 +31,15 @@ function Dashboard() {
       <style jsx global>{`
         input.input.dash-search {
           height: 55px;
-          //...
+          border: 1px solid #6f4195;
+
+          :focus {
+            border-color: #8b36bb;
+          }
+
+          ::selection {
+            background: #6f4195;
+          }
         }
       `}</style>
     </>
