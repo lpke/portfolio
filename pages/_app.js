96cc948a739e2c412c89b788e655b7e6af36515c
Luke Perich <luke@lpdev.io>
2021-01-28 11:33:20 +1100

    Completely updated framework parts and reset to a modern base

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,42 @@
-import 'styles/global.scss';
-import TagManager from 'react-gtm-module';
+import { React, useState, useEffect } from "react";
+import Head from "next/head";
+import "styles/global.scss";
+import { ViewportProvider } from "lib/hooks/use-viewport";
+import { SWRConfig } from 'swr'; // SWR global config
+import fetchJson from "lib/fetchJson"; // custom fetch wrapper
 
-class App extends React.Component {
-  componentDidMount() {
-    /* Google Tag Manager */
-    const tagManagerArgs = {
-      gtmId: 'GTM-KDQ9GWC'
-    };
-    TagManager.initialize(tagManagerArgs);
-  }
-  
-  render() {
-    let Component = this.props.Component;
-    let pageProps = this.props.pageProps;
 
-    return (
-      <>
-        <Component {...pageProps} />
-      </>
-    );
-  }
+const createTitle = (pageName) => {
+  const websiteName = "LPDev";
+  return pageName ? `${websiteName} - ${pageName}` : websiteName;
+};
+
+function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>{createTitle(pageProps.pageName || Component.name)}</title>{" "}
+        <link rel="icon" href="/icons/favicons/default-favicon.ico" />
+      </Head>
+
+      <SWRConfig
+        value={{
+          fetcher: fetchJson,
+          onError: (err) => {
+            console.error(err);
+          },
+        }}
+      >
+        <ViewportProvider>
+          <Component {...pageProps} />
+        </ViewportProvider>
+      </SWRConfig>
+
+      <style jsx>{`
+        //...
+      `}</style>
+    </>
+  );
 }
 
 export default App;
--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,17 @@
-import Link from 'next/link';
-import BasicContentArea from 'components/basic-content';
-import BasicFooter from 'components/basic-footer';
+import BasicFooter from "components/basic-footer";
+import Center from "layouts/BlankCenter";
 
 const Home = () => (
   <>
-    <BasicContentArea>
-      
-      <h1>Next.js Testing</h1>
+    <Center>
+      <h1>LP Dev</h1>
 
-      <p>
-        This website will be the future home of my portfolio, currently live at <a href="https://lpdev.io" target="_blank">lpdev.io</a>
-      </p>
-      
-      <Link href="internal-page">
-        <a className="button">Example Page</a>
-      </Link>
+      <p>Website currently undergoing improvements.</p>
 
       <style jsx>{`
-        
+        //...
       `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-            Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    
-    </BasicContentArea>
+    </Center>
 
     <BasicFooter />
   </>
