036ead4a75e91c31fa08c26e7a836bd05b221f97
Luke <luke@lpdev.io>
2026-03-24 16:39:49 +1100

    config: add MDX

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
-import type { NextConfig } from "next";
+import nextMDX from '@next/mdx';
+import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const withMDX = nextMDX({ extension: /\.mdx?$/ });
+
+const nextConfig: NextConfig = withMDX({
+  reactStrictMode: true,
   reactCompiler: true,
-};
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  images: {
+    deviceSizes: [
+      320, 420, 768, 1024, 1200, 1440, 1920, 2560, 3000, 3500, 4000,
+    ],
+  },
+});
 
 export default nextConfig;
